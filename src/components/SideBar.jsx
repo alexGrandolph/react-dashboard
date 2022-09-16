880c24aee63ebc0e36ebe4d507f235827d5d8223
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-16 13:37:48 -0600

    docs: add import to Sidebar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+import { SiShopware } from 'react-icons/si'
+import { MdOutlineCancle } from 'react-icons/md'
+import { TooltipComponent } from'@syncfusion/ej2-react-popups'
+
+import { links } from '../data/dummy'
 
 const SideBar = () => {
   return (
