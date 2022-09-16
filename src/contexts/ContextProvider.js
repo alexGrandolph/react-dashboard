3bc45a27a69f926c7598583faee222e54eaba6de
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-16 14:07:15 -0600

    feat: add ContextProvider.js for activeMenu state

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext()
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notifications: false
+}
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  
+  return (
+    <StateContext.Provider
+      value={{ 
+        activeMenu // if k/v pair has same name: don't need activeMenu: activeMenu
+      }}
+    >
+      {children}
+
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext);
