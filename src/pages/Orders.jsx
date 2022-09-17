c2d6dfc0b7649aa055b3b9332dab63e0a19bb432
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-17 12:36:34 -0600

    feat: add columngrid component to Orderspage

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,8 +5,19 @@ import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>
+    // <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl ">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders"/>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+      >
+        <ColumnsDirective>
+        {ordersGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
