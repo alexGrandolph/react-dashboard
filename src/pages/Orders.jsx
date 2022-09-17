3f05599bcc73c80297eebf0b212c9f2cf5ce6d09
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-17 12:40:46 -0600

    feat: add pagination, sorting, and export to Orders chart/grid

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -11,12 +11,17 @@ const Orders = () => {
       <GridComponent
         id="gridcomp"
         dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
       >
         <ColumnsDirective>
         {ordersGrid.map((item, index) => (
           <ColumnDirective key={index} {...item}/>
         ))}
         </ColumnsDirective>
+        <Inject services={[ Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport ]}/>
       </GridComponent>
     </div>
   )
